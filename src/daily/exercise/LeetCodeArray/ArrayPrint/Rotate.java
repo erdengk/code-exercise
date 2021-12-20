42658cbf4a8d754ea813b843e514133d1da13832
Kai Dang <15596570256@163.com>
2021-12-20 12:24:44 +0800

    :rocket: update README and some questions

--- src/daily/exercise/LeetCodeArray/ArrayPrint/Rotate.java
+++ src/daily/exercise/LeetCodeArray/ArrayPrint/Rotate.java
@@ -15,7 +15,30 @@ public class Rotate {
      * @param matrix
      */
     public static void rotate( int[][] matrix ) {
+        int x = 0;
+        int y = 0;
+        int endX = matrix.length - 1;
+        int endY = matrix[0].length - 1;
+        while ( x < endX ) {
+            rotateArr( matrix, x++, y++, endX--, endY-- );
+        }
+    }
 
+
+    private static void rotateArr( int[][] matrix, int x, int y, int endX, int endY ) {
+        int temp = 0;
+        for ( int i = 0; i < endY - y; i++ ) {
+            //保存第一行的数
+            temp = matrix[x][y + i];
+            // 将第一列的数给第一行
+            matrix[x][y + i] = matrix[endX - i][y];
+            // 将最后一行的数 给第一列
+            matrix[endX - i][y] = matrix[endX][endY - i];
+            // 将最后一列的数  给最后一行
+            matrix[endX][endY - i] = matrix[x + i][endY];
+            // 将 第一行的数 给最后一列
+            matrix[x + i][endY] = temp;
+        }
     }
 
 
--- src/daily/exercise/LeetCodeArray/window/CheckInclusion.java
+++ src/daily/exercise/LeetCodeArray/window/CheckInclusion.java
@@ -11,34 +11,35 @@ import java.util.Arrays;
 
 public class CheckInclusion {
 
+    /**
+     * [567. 字符串的排列](https://leetcode-cn.com/problems/permutation-in-string/)
+     * @param s1
+     * @param s2
+     * @return
+     */
     public boolean checkInclusion( String s1, String s2 ) {
         boolean b = false;
-        int []a1= new int[26];
-        int []a2= new int[26];
-        int l=0;
-        int r=0;
+        int[] a1 = new int[26];
+        int[] a2 = new int[26];
+        int l = 0;
+        int r = 0;
         for ( int i = 0; i < s1.length(); i++ ) {
-            a1[s1.charAt( i )-'a']++;
+            a1[s1.charAt( i ) - 'a']++;
         }
-        while ( r<s2.length() ){
-            int index = s1.charAt( r )-'a';
-            if(a1[index]!=0){
-                a2[index]++;
-                r++;
-            }else{
-                l++;
-                r++;
+        while ( r < s2.length() ) {
+            int index = s2.charAt( r ) - 'a';
+            a2[index]++;
+            if ( r - l + 1 == s1.length() ) {
+                if ( check( a1, a2 ) ) {
+                    return true;
+                } else {
+                    a2[s2.charAt( l ) - 'a']--;
+                    l++;
+                }
             }
-            if(r-l+1==s1.length()){
-                b = check(a1,a2);
-            }
-            if(b){
-                return b;
-            }else{
-                l=r;
-            }
-        }
+            r++;
 
+        }
         return b;
     }
 
@@ -46,7 +47,9 @@ public class CheckInclusion {
     private boolean check( int[] a1, int[] a2 ) {
         Boolean f = true;
         for ( int i = 0; i < a1.length; i++ ) {
-            if(a1[i]!=a2[i])return false;
+            if ( a1[i] != a2[i] ) {
+                return false;
+            }
         }
         return f;
     }
