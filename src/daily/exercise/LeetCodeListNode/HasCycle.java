7ad71f3b3955e52e155df8c7bf3e0f763ad481aa
Kai Dang <15596570256@163.com>
2021-12-25 15:49:48 +0800

    :rocket: update README and ListNode questions

--- src/daily/exercise/LeetCodeListNode/HasCycle.java
+++ src/daily/exercise/LeetCodeListNode/HasCycle.java
@@ -17,14 +17,83 @@ public class HasCycle {
     public boolean hasCycle(ListNode head) {
         if(head==null || head.next==null) return false;
         ListNode slow = head;
-        ListNode fast = head.next;
+        ListNode fast = head;
 
         while ( fast.next!=null && fast.next.next!=null){
-            if(slow==fast)return true;
             slow=slow.next;
-            fast=fast.next;
+            fast=fast.next.next;
+            if(slow==fast)return true;
+        }
+        return false;
+    }
+    public static boolean hasCycleTest(ListNode head) {
+        if(head==null || head.next==null) return false;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while ( fast.next!=null && fast.next.next!=null && fast.next.next.next!=null){
+
+            System.out.println("slow.val="+slow.val+"   fast.val="+fast.val);
+            slow=slow.next;
+            fast=fast.next.next.next;
+            if(slow==fast){
+                System.out.println("slow.val="+slow.val+"   fast.val="+fast.val);
+                return true;
+            }
         }
         return false;
     }
 
+
+    public static void main( String[] args ) {
+        ListNode node1  = new ListNode(1);
+        ListNode node2  = new ListNode(2);
+        ListNode node3  = new ListNode(3);
+        ListNode node4  = new ListNode(4);
+        ListNode node5  = new ListNode(5);
+        ListNode node6 = new ListNode(6);
+        node1.next=node2;
+        node2.next=node3;
+        node3.next=node4;
+        node4.next=node5;
+        node5.next=node6;
+        node6.next=node3;
+        System.out.println( hasCycleTest( node1 ) );
+    }
+
+
+    /**
+     1.判断是否有环用快慢指针，只要相遇即为环，不用说，好理解。
+     2.求环入口，就是一个数学问题转换为代码的问题。
+                     -------------------
+                     |                 ^
+                     v                 |
+     0->1->2->3-> …… -> A ->....... -> B
+     假设A为环入口，B为相遇点，设0到A距离为x,A到B距离为y,环的长度为c，快慢指针相遇是慢指针绕环n圈，快指针绕环m圈，
+     由条件得快慢指针相遇时快指针走的长度是慢指针的2倍，则：
+     2(x+nc+y)=x+mc+y；
+     化简得x+y=(m-2n)c；
+     x= （m-2n）c- y
+     设相遇点到环入口距离为 z = c -y  y= c -z
+     x  = （m-2n）c - (c-z)
+     x  = (m-2n-1)c + z
+
+     这意味着从起点0相遇点B的长度为环长度的正整数倍；
+     换句话说，就是现在让两个指针速度都变成1(重点！！！)，第一个指针从起点0出发，第二个指针从相遇点B出发，
+     则两个指针最后一定会在B点相遇；
+     但这是两个指针第一次相遇吗？
+     不，因为两个指针速度是相同的，所以往前退一退，就会发现两个指针其实是在环的入口第一次相遇后，就一直重合了；
+     所以代码就转换成两个速度为1的指针，一个从起点出发，一个从B点出发，第一次相遇的节点即为入环点。
+     伪代码如下：
+     ListNode q = head;
+     while (p != q) {
+     p = p.next;
+     q = q.next;
+     }
+     return q;`
+     **/
+
+    //TODO
+    // 其他解题思路
+    // https://leetcode-cn.com/problems/linked-list-cycle/solution/3chong-jie-jue-fang-shi-liang-chong-ji-bai-liao-10/
 }
--- src/daily/exercise/LeetCodeListNode/IsPalindrome.java
+++ src/daily/exercise/LeetCodeListNode/IsPalindrome.java
@@ -28,8 +28,9 @@ public class IsPalindrome {
         ListNode reverseStart = slow;
         ListNode node = head;
         // 如果是奇数个节点，此时fast在最后一个节点
-        // 需要将slow 指向下一个节点
+        // 可以用fast的下一个节点是否为空来判断整个链表节点个数是技术还是偶数
         if ( fast.next == null ) {
+            // 需要将slow 指向下一个节点 再进行翻转
             slow = slow.next;
             ListNode temp = reverseList( slow);
             while ( temp!=null ){
@@ -42,11 +43,12 @@ public class IsPalindrome {
             }
             reverseStart.next = reverseList( slow );
         } else {
-            //返回 fast为首的后半部分链表
+            // 若节点个数为偶数，则slow 在上中点
+            // 需要返回以下中点为首的 翻转后的链表
+            //返回 slow.next后半部分链表
             ListNode temp = reverseList( slow.next );
             while ( temp!=null ){
                 if(temp.val!=node.val){
-
                     return false;
                 }
                 temp=temp.next;
@@ -72,6 +74,35 @@ public class IsPalindrome {
             cur = temp;
         }
         return pre;
+    }
+
+
+    /**
+     * 优秀的解法
+     * https://labuladong.gitee.io/algo/2/17/19/
+     * @param head
+     * @return
+     */
+    boolean isPalindromeByLa(ListNode head) {
+        ListNode slow, fast;
+        slow = fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        if (fast != null)
+            slow = slow.next;
+
+        ListNode left = head;
+        ListNode right = reverseList(slow);
+        while (right != null) {
+            if (left.val != right.val)
+                return false;
+            left = left.next;
+            right = right.next;
+        }
+        return true;
     }
 
 }
