0d3e0596ec0bb97e12ea256239159ac553f8bf9e
erdengk <15596570256@163.com>
2022-03-02 13:08:00 +0800

    :rocket: update some dynamic question

---
+++ src/daily/exercise/LeetCodeDynamicProgram/Bag/LastStoneWeightII.java
@@ -0,0 +1,47 @@
+package daily.exercise.LeetCodeDynamicProgram.Bag;
+
+/**
+ * @ClassName: LastStoneWeightII
+ * @Description:
+ * @author: dk
+ * @date: 2022/3/2 1:07 下午
+ */
+
+public class LastStoneWeightII {
+
+    /**
+     * 1049. 最后一块石头的重量 II
+     * https://leetcode-cn.com/problems/last-stone-weight-ii/
+     * @param stones
+     * @return
+     */
+    public int lastStoneWeightII(int[] stones) {
+        int sum = 0;
+        for (int s : stones) {
+            sum += s;
+        }
+
+        int target = sum / 2;
+        //初始化，dp[i][j]为可以放0-i物品，背包容量为j的情况下背包中的最大价值
+        int[][] dp = new int[stones.length][target + 1];
+        //dp[i][0]默认初始化为0
+        //dp[0][j]取决于stones[0]
+        for (int j = stones[0]; j <= target; j++) {
+            dp[0][j] = stones[0];
+        }
+
+        for (int i = 1; i < stones.length; i++) {
+            for (int j = 1; j <= target; j++) {//注意是等于
+                if (j >= stones[i]) {
+                    //不放:dp[i - 1][j] 放:dp[i - 1][j - stones[i]] + stones[i]
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - stones[i]] + stones[i]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        // System.out.println(dp[stones.length - 1][target]);
+        return (sum - dp[stones.length - 1][target]) - dp[stones.length - 1][target];
+    }
+}
