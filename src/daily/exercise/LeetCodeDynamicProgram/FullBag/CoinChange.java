cdd0ae0e47e65ed6343e5ed81f65841cd4ab1ab4
erdengk <15596570256@163.com>
2022-03-07 16:11:09 +0800

    :rocket: update some dynamic question

--- src/daily/exercise/LeetCodeDynamicProgram/FullBag/CoinChange.java
+++ src/daily/exercise/LeetCodeDynamicProgram/FullBag/CoinChange.java
@@ -16,9 +16,28 @@ public class CoinChange {
      * @param amount
      * @return
      */
-    public int coinChange(int[] coins, int amount) {
+    public int change(int[] coins, int amount) {
         return  0 ;
     }
 
+    int res = 0 ;
+    public int changeByBack(int amount, int[] coins) {
+        get(coins,amount,0,0);
+        return res;
+    }
+    public void get(int []coins,int amount,int target,int index){
+        if(target==amount){
+            res++;
+            return;
+        }
+        if(target>amount)return;
 
+        for(int i=index;i<coins.length;i++){
+
+            target+=coins[i];
+            get(coins,amount,target,i);
+            target-=coins[i];
+
+        }
+    }
 }
